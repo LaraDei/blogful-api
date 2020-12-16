f34b8ea962e60b22d55a3c5f5c1b30e6f827fb29
Lara Samuelson <lara.samuelson@gmail.com>
2020-12-16 11:23:50 -0800

    data design changes and test update

--- src/app.js
+++ src/app.js
@@ -4,19 +4,31 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const articlesRouter = require('./articles/articles-router')
+const usersRouter = require('./users/users-router')
+const commentsRouter = require('./comments/comments-router')
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
+const morganOption = ((NODE_ENV === 'production')
   ? 'tiny'
-  : 'common';
+  : 'common',
+  {
+    skip: () => NODE_ENV === 'test'
+  })
 
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('Hello, boilerplate!')
+app.use('/api/articles', articlesRouter)
+
+app.use('/api/users', usersRouter)
+
+app.use('/api/comments', commentsRouter)
+
+app.get('/api', (req, res) => {
+    res.send('Hello, world!')
 })
 
 app.use(function errorHandler(error, req, res, next) {
